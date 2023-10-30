37f40f0aeb99a93e2f08e6b157d720497124d991
mateusz-lisowski <mateusz.lisowski.dev@gmail.com>
2023-10-30 18:35:59 +0100

    Refactored decimal reading code to dedicated function

--- task32.asm
+++ task32.asm
@@ -13,6 +13,36 @@ user_input db MAX_SIZE dup (?)  ; Declare space for user input in memory
 ten db 10                       ; Set 10 in memory for multiplication purposes
 
 .code
+
+read_dec_num_to_eax PROC
+    pusha
+
+    ; Set global registers 
+    mov esi, 0
+    mov eax, 0
+
+    ; Convert number from decimal to binary
+    convert:
+        mov bl, user_input[esi]     ; Read character to bl
+        inc esi                     ; At the same time increment esi (for you not to forget to change it later)
+
+        cmp bl, 10                  ; Compare bl to 10 (enter char) 
+        je finish                   ; If the char is entern, finish 
+
+        sub bl, 30H                 ; Convert ASCII codes for numbers to the actual number
+        movzx ebx, bl               ; Expand the number to whole ebx
+
+        mul TEN                     ; Multiply result by 10
+        add eax, ebx                ; Add newly find number to eax
+        
+        jmp convert
+
+    finish:                         ; Finish function execution
+        popa
+        ret
+
+read_dec_num_to_eax ENDP
+
 _main PROC
     
     ; Read the user number
@@ -22,27 +52,9 @@ _main PROC
     call __read
     add esp, 12
 
-    ; Set global registers (set after function call, because it can change registers' values)
-    mov esi, 0
-    mov eax, 0
+    ; Call custom function
+    call read_dec_num_to_eax
 
-; Convert number from decimal to binary
-convert:
-    mov bl, user_input[esi]     ; Read first character to bl
-    inc esi                     ; At the same time increment esi (for you not to forget to change it later)
-
-    cmp bl, 10                  ; Compare bl to 10 (enter char) 
-    je finish                   ; If the char is entern, finish 
-
-    sub bl, 30H                 ; Convert ASCII codes for numbers to the actual number
-    movzx ebx, bl               ; Expand the number to whole ebx
-
-    mul TEN                     ; Multiply result by 10
-    add eax, ebx                ; Add newly find number to eax
-    
-    jmp convert
-
-finish:
     ; End of the program
     push 0
     call _ExitProcess@4
