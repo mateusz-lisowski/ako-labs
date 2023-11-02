e1ae300cfc028d396220fa252e98d75f70905ed8
mateusz-lisowski <mateusz.lisowski.dev@gmail.com>
2023-11-02 22:45:38 +0100

    Reworked task no. 2.5 to transform polish specific characters also

--- lab2/task25.asm
+++ lab2/task25.asm
@@ -43,6 +43,33 @@ _main PROC
 
         mov al, user_input[esi]     ; Move character from user_input to al
 
+        cmp al, 0A5H                ; Check for 'ą' character
+        je change_a
+
+        cmp al, 086H                ; Check for 'ć' character
+        je change_c
+
+        cmp al, 0A9H                ; Check for 'ę' character
+        je change_e
+
+        cmp al, 088H                ; Check for 'ł' character
+        je change_l
+
+        cmp al, 0E4H                ; Check for 'ń' character
+        je change_n
+
+        cmp al, 0A2H                ; Check for 'ó' character
+        je change_o
+
+        cmp al, 098H                ; Check for 'ś' character
+        je change_s
+
+        cmp al, 0ABH                ; Check for 'ź' character
+        je change_z1
+
+        cmp al, 0BEH                ; Check for 'ż' character
+        je change_z2
+
         cmp al, 'a'                 ; Check if character is smaller than 'a'
         jb skip                     ; If yes, it does not need to be changed, so skip
 
@@ -50,6 +77,90 @@ _main PROC
         ja skip                     ; If yes, it does not need to be changed, so skip
 
         sub al, 20H                 ; Transform small letters into big ones
+        mov user_input[esi], al     ; Save transformed character to memory
+
+    change_a:                       ; Change 'ą' to 'Ą'
+
+        cmp al, 0A5H
+        jne save
+
+        mov al, 0A4H
+        jmp save
+
+    change_c:                       ; Change 'ć' to 'Ć'
+
+            cmp al, 086H
+            jne save
+
+            mov al, 08FH
+            jmp save
+
+
+    change_e:                       ; Change 'ę' to 'Ę'
+
+            cmp al, 0A9H
+            jne save
+
+            mov al, 0A8H
+            jmp save
+
+
+    change_l:                       ; Change 'ł' to 'Ł'
+
+            cmp al, 088H
+            jne save
+
+            mov al, 09DH
+            jmp save
+
+
+    change_n:                       ; Change 'ń' to 'Ń'
+
+            cmp al, 0E4H
+            jne save
+
+            mov al, 0E3H
+            jmp save
+
+            
+
+    change_o:                       ; Change 'ó' to 'Ó'
+
+            cmp al, 0A2H
+            jne save
+
+            mov al, 0E0H
+            jmp save
+
+
+    change_s:                       ; Change 'ś' to 'Ś'
+
+            cmp al, 098H
+            jne save
+
+            mov al, 097H
+            jmp save
+
+
+    change_z1:                      ; Change 'ź' to 'Ź'
+
+            cmp al, 0ABH
+            jne save
+
+            mov al, 08DH
+            jmp save
+
+
+    change_z2:                      ; Change 'ż' to 'Ż'
+
+            cmp al, 0BEH
+            jne save
+
+            mov al, 0BDH
+            jmp save
+
+    save:                           
+
         mov user_input[esi], al     ; Save transformed character to memory
 
     skip: 
