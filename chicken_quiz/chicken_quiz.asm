cb6875700657428a6d487ce3993d03cfc9f81572
mateusz-lisowski <mateusz.lisowski.dev@gmail.com>
2023-11-06 23:54:15 +0100

    Function for showing user congrats

--- chicken_quiz/chicken_quiz.asm
+++ chicken_quiz/chicken_quiz.asm
@@ -14,13 +14,27 @@ STDIN equ 0
 
 too_much db 'Nope, there are less chickens in the hut', 10, 0
 too_little db 'Nope, there are more chickens in the hut', 10, 0
+congrats db 'Exacly, there are this much chickens in the hut', 10, 0
 
 .code
+
+print_congrats PROC
+    pusha                       ; Save registers state
+
+    push LENGTHOF congrats      ; Calculate size of congrats
+    push OFFSET congrats        ; Set too_much label as place to start writing
+    push STDOUT                 ; Set out to stdout 
+    call __write                ; Call write function from C library
+    add esp, 12                 ; Reset write function parameters
+
+    popa                        ; Restore registers state
+    ret                         ; Return from the function
+print_congrats ENDP
 
 print_to_many_chikens PROC
     pusha                       ; Save registers state
 
-    push LENGTHOF too_much      ; Set size of write to 255 (MAX_HINT_SIZE)
+    push LENGTHOF too_much      ; Calculate size of too_much
     push OFFSET too_much        ; Set too_much label as place to start writing
     push STDOUT                 ; Set out to stdout 
     call __write                ; Call write function from C library
@@ -33,7 +47,7 @@ print_to_many_chikens ENDP
 print_to_little_chikens PROC
     pusha                       ; Save registers state
 
-    push LENGTHOF too_little    ; Set size of write to 255 (MAX_HINT_SIZE)
+    push LENGTHOF too_little    ; Calculate size of too_little
     push OFFSET too_little      ; Set too_little label as place to start writing
     push STDOUT                 ; Set out to stdout 
     call __write                ; Call write function from C library
@@ -47,6 +61,7 @@ _main PROC
 
     call print_to_many_chikens
     call print_to_little_chikens
+    call print_congrats
 
     ; End program
     push 0
