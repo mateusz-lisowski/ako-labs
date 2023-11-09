c919d13778dffe03ae7338baf3bb5d713acc0d3a
mateusz-lisowski <mateusz.lisowski.dev@gmail.com>
2023-11-09 14:10:45 +0100

    Refactored random genarator function and added comments

--- chicken_quiz/random_number.asm
+++ chicken_quiz/random_number.asm
@@ -3,33 +3,32 @@
 
 .code
 
-generate_random_number_to_eax_from_eax PROC
+; Generates random number (always divisible by 4 :/ unfortunately) from 0 to given argument
+generate_random_number PROC
 
-    ; Save registers state
-    push ebx 
-    push ecx 
+    push ebp            ; Save the base pointer
+    mov ebp, esp        ; Set up a new base pointer
+    
+    ; Save any registers that you plan to modify
+    push ebx            
     push edx
-    push esi 
-    push edi
-    push ebp
 
-    mov ebx, eax
-    mov eax, esp
+    ; Retrieve the argument from the stack
+    mov ebx, [ebp+8]    ; Argument was the first pushed (8 bytes above the base pointer)
+    mov eax, esp        ; Choose some random number (ESP is different every time, but always divides by 4)
 
-    xor edx, edx
-    div ebx
+    xor edx, edx        ; Zero EDX before the division
+    div ebx             ; Divide by argument to ger the rest (modulo operation)
 
-    mov eax, edx
+    mov eax, edx        ; Move result to EAX
 
-    ; Restore registers state
-    pop ebp 
-    pop edi 
-    pop esi 
-    pop edx 
-    pop ecx 
+    ; Restore registers and clean up the stack
+    pop edx
     pop ebx
+    pop ebp
 
     ret
-generate_random_number_to_eax_from_eax ENDP
+
+generate_random_number ENDP
 
 END
