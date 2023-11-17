9e0dacdc398ef15688c9837ec1ef1502aba28366
mateusz-lisowski <mateusz.lisowski.dev@gmail.com>
2023-11-17 17:27:07 +0100

    Rewrited random number function to get sytem time as a seed

--- chicken_quiz/random_number.asm
+++ chicken_quiz/random_number.asm
@@ -1,29 +1,40 @@
 .686
 .model flat
 
+extern _GetSystemTime@4 : PROC          ; Get function for checking system time from Win32 api
+
+.data
+
+time_struct dw 8 dup (?)                ; Reserve space for time structure returned by GetSystemTime function
+
 .code
 
 ; Generates random number (always divisible by 4 :/ unfortunately) from 0 to given argument
 generate_random_number PROC
 
-    push ebp            ; Save the base pointer
-    mov ebp, esp        ; Set up a new base pointer
-    
+    push ebp                            ; Save the base pointer
+    mov ebp, esp                        ; Set up a new base pointer
+
     ; Save any registers that you plan to modify
     push ebx            
-    push edx
+
+    ; Obtain current system time
+    push offset time_struct             ; First argument is pointer to the memory 
+    call _GetSystemTime@4               ; Call the GetSystemTime function
+
+    ; Generate random number from time
+    mov eax, dword PTR time_struct[12]  ; Move number of seconds from time_struct to EAX
+    add eax, dword PTR time_struct[14]  ; Add number of milliseconds from time_struct to EAX
 
     ; Retrieve the argument from the stack
-    mov ebx, [ebp+8]    ; Argument was the first pushed (8 bytes above the base pointer)
-    mov eax, esp        ; Choose some random number (ESP is different every time, but always divides by 4)
+    mov ebx, [ebp+8]                    ; Argument was the first pushed (8 bytes above the base pointer)
 
-    xor edx, edx        ; Zero EDX before the division
-    div ebx             ; Divide by argument to ger the rest (modulo operation)
+    xor edx, edx                        ; Zero EDX before the division
+    div ebx                             ; Divide by argument to ger the rest (modulo operation)
 
-    mov eax, edx        ; Move result to EAX
+    mov eax, edx                        ; Move result to EAX
 
     ; Restore registers and clean up the stack
-    pop edx
     pop ebx
     pop ebp
 
