29b44f257de6c0c49efa9487e6c89c21b659db47
mateusz-lisowski <mateusz.lisowski.dev@gmail.com>
2023-11-18 23:21:44 +0100

    Calculation working for two arguments (only adding)

--- onp_calculator/calculate_onp.asm
+++ onp_calculator/calculate_onp.asm
@@ -7,24 +7,89 @@ extern __write : PROC
 
 .code
 
+; Function for checking if given character is an ONP operand
+is_operand PROC
+
+    ; Standard function prolog
+    push ebp            ; Save EBP on stack
+    mov ebp, esp        ; Move current value of ESP to EPB
+
+    mov eax, [ebp + 8]  ; Move address of user formula to EBP
+
+    cmp eax, '+'        ; Check if char is a plus sign
+    je operand          ; If yes it is an ONP operand
+
+    cmp eax, '-'        ; Check if char is a minus sign
+    je operand          ; If yes it is an ONP operand
+
+    cmp eax, '*'        ; Check if char is a multiplication sign
+    je operand          ; If yes it is an ONP operand
+
+    cmp eax, '/'        ; Check if char is a division sign
+    je operand          ; If yes it is an ONP operand
+
+    ; If neiter of above is true, then sign is not an ONP operand
+    mov eax, 0          ; If character is not an opernad, then return false (in asm equivalent to returning false is setting EAX to 0)
+    jmp finish          ; Finish function execution
+
+    operand:
+        mov eax, 1      ; If character is an opernad, then return true (in asm equivalent to returning true is setting EAX to 1)
+
+    finish:
+
+    ; Standard function epilog
+    pop ebp             ; Restore EBP from stack
+    ret                 ; Retutn to current value of ESP
+
+is_operand ENDP
+
+; Function for calcultating ONP
 calculate_onp PROC
-    push ebp
-    mov ebp, esp
-
-    push ebx
     
+    ; Standard function prolog
+    push ebp            ; Save EBP on stack
+    mov ebp, esp        ; Move current value of ESP to EPB
 
-    mov ebx, [ebp + 8] 
+    sub esp, 8          ; Reserve memory for local variables
 
-    push 255
+    ; Save used registers on stack
     push ebx
-    push 1
-    call __write
-    add esp, 12
+    push esi
 
+    mov ebx, [ebp + 8]  ; Move address of user formula to EBP
+    mov esi, 0
+
+    calculate:
+
+        mov eax, [ebx + esi]
+        inc esi
+        mov edx, eax
+
+        push eax
+        call is_operand
+        add esp, 4
+
+        cmp eax, 1
+        je char_is_operand
+
+        push edx
+        jmp calculate
+
+    char_is_operand:
+
+        pop ecx
+        pop edx
+        mov eax, 0
+        add eax, ecx
+        add eax, edx
+
+    ; Restore registers state
+    pop esi
     pop ebx
 
-    pop ebp
-    ret
+    ; Standard function epilog
+    pop ebp             ; Restore EBP from stack
+    ret                 ; Return to current value of ESP
+
 calculate_onp ENDP
 END
